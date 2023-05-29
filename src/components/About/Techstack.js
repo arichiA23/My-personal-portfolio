3c273f065b0d57617d816a56c6f939c00e4e6bee
arichiA23 <128364835+arichiA23@users.noreply.github.com>
2023-05-30 00:57:35 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,45 +8,101 @@ import {
   DiMongodb,
   DiPython,
   DiGit,
+  DiDotnet,
+  DiFirebase,
+  DiCss3,
+  DiHtml5,
+  
 } from "react-icons/di";
 import {
   SiR,
   SiCss3,
   SiHtml5,
+  SiFirebase,
+  SiDotnet,
+  SiPython,
+  SiGit,
+  SiMongodb,
+  SiReact,
+  SiNodedotjs,
+  SiJavascript,
+  SiCplusplus
 } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+        <SiCplusplus/>
+        <h6 className="name">
+          C++
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        <SiJavascript />
+        <h6 className="name">
+          JavaScript
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <h6 className="name">
+          Node.js
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <h6 className="name">
+          React.js
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
+        <SiMongodb />
+        <h6 className="name">
+          Mongodb
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
+        <DiHtml5 />
+        <h6 className="name">
+          HTML
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <h6 className="name">
+        Git
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCss3 />
+        <h6 className="name">
+        CSS
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <h6 className="name">
+          Python
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiR/>
+        <h6 className="name">
+        R Language
+        </h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDotnet/>
+        <h6 className="name">
+        .Net
+        </h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase/>
+        <h6 className="name">
+        Firebase
+        </h6>
       </Col>
     </Row>
   );
