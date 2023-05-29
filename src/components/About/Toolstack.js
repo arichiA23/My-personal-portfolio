a2eecedb0e24cedb554b53024c5fab7d0eee3beb
arichiA23 <128364835+arichiA23@users.noreply.github.com>
2023-05-30 00:54:55 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -8,35 +8,54 @@ import {
   SiRstudio,
   SiJupyter,
   SiMysql,
-  SiOracle
+  SiOracle,
+  SiSlack
 } from "react-icons/si";
+import {
+  DiLinux,
+  DiVisualstudio,
+  DiMysql,
+  
+} from "react-icons/di";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiKalilinux />
+        <h6 className="name"> Kali Linux</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
+        <DiLinux />
+        <h6 className="name">Linux</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <h6 className="name">Visual Studio Code</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVirtualbox />
+        <h6 className="name">Virtual Box</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRstudio />
+        <h6 className="name">R Studio</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJupyter />
+        <h6 className="name">Jupyter Notebook</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
+        <DiMysql />
+        <h6 className="name">MySQL</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiOracle />
+        <h6 className="name">Oracle</h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSlack />
+        <h6 className="name">Slack</h6>
       </Col>
     </Row>
     
