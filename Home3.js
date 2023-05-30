4e8c9a4c8012f717b129471da202e5e054813ffc
arichiA23 <128364835+arichiA23@users.noreply.github.com>
2023-05-31 00:19:59 +0530

    Add files via upload

---
+++ Home3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from './contactform/ContactForm';
+
+function App() {
+  return (
+    <div>
+      
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
