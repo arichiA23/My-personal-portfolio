098336ba0e5d8716cdaba1621d2335afba504291
arichiA23 <128364835+arichiA23@users.noreply.github.com>
2023-05-31 02:11:03 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -17,6 +18,7 @@ function ResumeNew() {
   return (
     <div>
       <Container fluid className="resume-section">
+      <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
